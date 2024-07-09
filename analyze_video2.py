e21357b304a09f4119d85f41da6d15d3ffef1801
Andy Tan Kar Hoe <andy.kar.hoe@outlook.com>
2024-07-09 14:09:26 +0800

    Update analyze_video2.py

--- analyze_video2.py
+++ analyze_video2.py
@@ -18,7 +18,7 @@ from PIL import Image
 
 def wide_space_default():
     st.set_page_config(layout="wide")
-wide_space_default
+wide_space_default()
 
 # Define functions
 def vidtoaudio(filename, output_name):
